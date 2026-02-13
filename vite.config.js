453f7ec285390065f39d7e03d7020b9f802ce29d
Imasha2006 <pimashaperera2006@gmail.com>
2026-02-13 09:39:28 +0530

    vite.conjig.js file change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,21 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
+
   ],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'src/pages/about.html',
+        community: 'src/pages/community.html',
+        product: 'src/pages/product.html',
+        subscribe: 'src/pages/subscribe.html',
+        '404': 'src/pages/404.html',
+      },
+    },
+  },
 })
